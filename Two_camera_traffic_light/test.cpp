a9acfe91f8f78d78af8e09bd95777d91848f152c
unknown <tharindu.samare123@gmail.com>
2021-12-15 14:23:21 +0530

    added new nms algorithm

--- Two_camera_traffic_light/test.cpp
+++ Two_camera_traffic_light/test.cpp
@@ -5,29 +5,37 @@
 #include <fstream>
 #include <iomanip>
 
-int main()
-{
-    std::fstream in("homography_matrix.txt");
-    std::string line;
-    int i = 0;
-    int j = 0;
-    float M[3][3]={};
+// int main()
+// {
+//     std::fstream in("homography_matrix.txt");
+//     std::string line;
+//     int i = 0;
+//     int j = 0;
+//     float M[3][3]={};
 
-    while (std::getline(in, line))
-    {
-        float value;
-        std::stringstream ss(line);
-        j = 0;
-        while (ss >> value){
-            M[i][j] = value;
-            j++;
-        }
-        ++i;
-    }
-    for(int x=0;x<3;x++){
-        for(int y=0;y<3;y++){
-            std::cout << std::fixed << std::setprecision(14) << M[x][y] << "  ";
-        }
-        std::cout << "\n";
-    }
+//     while (std::getline(in, line))
+//     {
+//         float value;
+//         std::stringstream ss(line);
+//         j = 0;
+//         while (ss >> value){
+//             M[i][j] = value;
+//             j++;
+//         }
+//         ++i;
+//     }
+//     for(int x=0;x<3;x++){
+//         for(int y=0;y<3;y++){
+//             std::cout << std::fixed << std::setprecision(14) << M[x][y] << "  ";
+//         }
+//         std::cout << "\n";
+//     }
+// }
+
+int main(){
+    
+
+
+
+    return 0;
 }
