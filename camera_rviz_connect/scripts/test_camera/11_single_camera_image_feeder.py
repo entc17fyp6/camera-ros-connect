5da17d33a82aec1533775e966d9345374102b2f4
unknown <tharindu.samare123@gmail.com>
2021-12-17 23:38:26 +0530

    added feature: added ffmpeg image feeder for inferencing camera output

---
+++ camera_rviz_connect/scripts/test_camera/11_single_camera_image_feeder.py
@@ -0,0 +1,319 @@
+import pypylon.pylon as py
+import numpy as np
+import matplotlib.pyplot as plt
+import traceback
+import time
+from datetime import datetime
+import cv2
+import subprocess as sp
+import os
+
+# import rospy
+# from sensor_msgs.msg import Image as CameraImage
+
+width = 1080
+height = 1920
+fps = 10
+narrow_AutoExposureTimeUpperLimit = 50000
+wide_AutoExposureTimeUpperLimit = 1000
+quality_factor = 30
+should_feed_video = True
+should_visualize = True
+
+### this FFMPEG_VideoWriter class is copied and modified from https://github.com/basler/pypylon/issues/113 
+
+### for demonstration of how to write video data
+### this class is an excerpt from the project moviepy https://github.com/Zulko/moviepy.git moviepy/video/io/ffmpeg_writer.py
+###
+
+class FFMPEG_Image_Feeder:
+    """ A class for FFMPEG-based video writing.
+
+    A class to write videos using ffmpeg. ffmpeg will write in a large
+    choice of formats.
+
+    Parameters
+    -----------
+
+    filename
+      Any filename like 'video.mp4' etc. but if you want to avoid
+      complications it is recommended to use the generic extension
+      '.avi' for all your videos.
+
+    size
+      Size (width,height) of the output video in pixels.
+
+    fps
+      Frames per second in the output video file.
+
+    codec
+      FFMPEG codec. It seems that in terms of quality the hierarchy is
+      'rawvideo' = 'png' > 'mpeg4' > 'libx264'
+      'png' manages the same lossless quality as 'rawvideo' but yields
+      smaller files. Type ``ffmpeg -codecs`` in a terminal to get a list
+      of accepted codecs.
+
+      Note for default 'libx264': by default the pixel format yuv420p
+      is used. If the video dimensions are not both even (e.g. 720x405)
+      another pixel format is used, and this can cause problem in some
+      video readers.
+
+      Experimentally found best options 
+        libx264         - quality - very good     speed - ~30fps achieved       size - 16.74 GB/h
+        libx265         - quality - very good     speed - ~15fps achieved       size - 1.396 GB/h
+        mjpeg(-q:v=25)  - quality - good          speed - ~30fps achieved       size - 3.66 GB/h
+        mpeg(-q:v=11)   - quality - very good     speed - ~30fps achieved       size - 1.624 GB/h
+        h264_qsv(-q:v=30) - quality - good        speed - ~30fps achieved       size - 1.6GB/h      -hardware acceleration
+ -
+    audiofile
+      Optional: The name of an audio file that will be incorporated
+      to the video.
+
+    preset
+      Sets the time that FFMPEG will take to compress the video. The slower,
+      the better the compression rate. Possibilities are: ultrafast,superfast,
+      veryfast, faster, fast, medium (default), slow, slower, veryslow,
+      placebo. 
+
+      This and affects only for the libx264, libx265 libxvid etc. ('-crf' also affect these types)
+      for mjpeg, mpeg4 etc. use -q:v factor
+
+    bitrate
+      Only relevant for codecs which accept a bitrate. "5000k" offers
+      nice results in general.
+
+    withmask
+      Boolean. Set to ``True`` if there is a mask in the video to be
+      encoded.
+
+    """
+
+    def __init__(self, size, fps, codec="libx264", audiofile=None,
+                 preset="medium", bitrate=None, pixfmt="rgba", quality = '11',crf = '20',
+                 logfile=None, threads=None, ffmpeg_params=None):
+
+        if logfile is None:
+            logfile = sp.PIPE
+
+        self.codec = codec
+        
+        # order is important\
+        cmd = [
+            "ffmpeg",
+            '-y',
+            '-loglevel', 'error' if logfile == sp.PIPE else 'info',
+            '-f', 'rawvideo',
+            '-vcodec', 'rawvideo',
+            '-s', '%dx%d' % (size[1], size[0]),
+            # '-pix_fmt', pixfmt,
+            '-r', '%.02f' % fps,
+            '-i', '-', '-an',
+            '-f', 'image2pipe',
+            '-pix_fmt', 'bgr24',
+            '-vcodec', codec,
+            '-q:v', quality,
+            '-preset', preset,
+            '-vf', "curves=r='0/0 0.25/0.4 0.5/0.5 1/1':g='0/0 0.25/0.4 0.5/0.5 1/1':b='0/0 0.25/0.4 0.5/0.5 1/1'",
+            # '-vf', "drawtext='fontfile=c\:/Windows/Fonts/Calibri.ttf:text=%{localtime}:fontcolor=yellow:fontsize=35:x=1600:y=20:'",
+            # '-vf', "curves=r='0/0 0.25/0.4 0.5/0.5 1/1':g='0/0 0.25/0.4 0.5/0.5 1/1':b='0/0 0.25/0.4 0.5/0.5 1/1', drawtext='fontfile=c\:/Windows/Fonts/Calibri.ttf:text=%{localtime}:fontcolor=yellow:fontsize=35:x=1600:y=20:'",
+            '-vcodec', 'rawvideo', '-an', '-',
+        ]
+
+        popen_params = {"stdout": sp.PIPE,
+                        "stderr": sp.PIPE,
+                        "stdin": sp.PIPE,
+                        # "shell":True   ## keep this line in windows 10, commentout in ubuntu 20.04
+                        }
+
+        # This was added so that no extra unwanted window opens on windows
+        # when the child process is created
+        if os.name == "nt":
+            popen_params["creationflags"] = 0x08000000  # CREATE_NO_WINDOW
+
+        self.proc = sp.Popen(cmd, **popen_params)
+
+
+    def view_frame(self, img_array):
+        """ Writes one frame in the file."""
+        try:
+            self.proc.stdin.write(img_array.tobytes())
+            raw_frame = self.proc.stdout.read(1920*1080*3)
+            frame = np.frombuffer(raw_frame, np.uint8)
+            frame = frame.reshape((1080, 1920, 3))
+            cv2.namedWindow("image", cv2.WINDOW_NORMAL)  
+            cv2.imshow('image', frame)
+            cv2.waitKey(1)
+        except IOError as err:
+            _, ffmpeg_error = self.proc.communicate()
+            error = (str(err) + ("\n\nMoviePy error: FFMPEG encountered "
+                                 "the following error while writing file %s:"
+                                 "\n\n %s" % (self.filename, str(ffmpeg_error))))
+
+            if b"Unknown encoder" in ffmpeg_error:
+
+                error = error+("\n\nThe video export "
+                  "failed because FFMPEG didn't find the specified "
+                  "codec for video encoding (%s). Please install "
+                  "this codec or change the codec when calling "
+                  "write_videofile. For instance:\n"
+                  "  >>> clip.write_videofile('myvid.webm', codec='libvpx')")%(self.codec)
+
+            elif b"incorrect codec parameters ?" in ffmpeg_error:
+
+                 error = error+("\n\nThe video export "
+                  "failed, possibly because the codec specified for "
+                  "the video (%s) is not compatible with the given "
+                  "extension (%s). Please specify a valid 'codec' "
+                  "argument in write_videofile. This would be 'libx264' "
+                  "or 'mpeg4' for mp4, 'libtheora' for ogv, 'libvpx for webm. "
+                  "Another possible reason is that the audio codec was not "
+                  "compatible with the video codec. For instance the video "
+                  "extensions 'ogv' and 'webm' only allow 'libvorbis' (default) as a"
+                  "video codec."
+                  )%(self.codec, self.ext)
+
+            elif  b"encoder setup failed" in ffmpeg_error:
+
+                error = error+("\n\nThe video export "
+                  "failed, possibly because the bitrate you specified "
+                  "was too high or too low for the video codec.")
+
+            elif b"Invalid encoder type" in ffmpeg_error:
+
+                error = error + ("\n\nThe video export failed because the codec "
+                  "or file extension you provided is not a video")
+
+
+            raise IOError(error)
+
+    def close(self):
+        if self.proc:
+            self.proc.stdin.close()
+            if self.proc.stderr is not None:
+                self.proc.stderr.close()
+            self.proc.wait()
+
+        self.proc = None
+
+    # Support the Context Manager protocol, to ensure that resources are cleaned up.
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+def image_feed(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2YUV_I420)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BayerBG2BGR)
+    feeder.view_frame(frame)
+    return
+
+def visualize(frame):
+    # frame= cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    cv2.namedWindow("Input")
+    cv2.imshow("Input", frame)
+    cv2.waitKey(1)
+    return
+
+def initialize_cam(cam,camera_name):
+    cam.UserSetSelector = 'Default'
+    cam.UserSetLoad.Execute()
+
+    # cam.PixelFormat = 'YCbCr422_8'
+    # cam.ExposureTime = 200
+    cam.PixelFormat = 'BayerGB8'
+    cam.ExposureTime = 300
+    cam.AcquisitionFrameRate = fps
+    # cam.BslBrightness = 0.4
+    # cam.BslContrast = 0.4
+    if (camera_name == 'Narrow'):
+        cam.ReverseX = True
+        cam.ReverseY = True
+        cam.ExposureAuto = 'Continuous'
+        cam.AutoExposureTimeUpperLimit = narrow_AutoExposureTimeUpperLimit
+        cam.AutoGainUpperLimit = 5.0
+
+        # cam.LightSourcePreset = 'Off'
+        # cam.BalanceWhiteAuto = 'Off'
+        # cam.Gain = 1
+        # cam.GainAuto = 'Off'
+        
+        # cam.BalanceRatioSelector = 'Red'
+        # cam.BalanceRatio = 2.1875
+        # cam.BalanceRatioSelector = 'Green'
+        # cam.BalanceRatio = 1.46875
+        # cam.BalanceRatioSelector = 'Blue'
+        # cam.BalanceRatio = 1.76
+
+    if (camera_name == 'Wide'):
+        cam.ExposureAuto = 'Continuous'
+        cam.AutoExposureTimeUpperLimit = wide_AutoExposureTimeUpperLimit
+        cam.AutoGainUpperLimit = 5.0
+        
+
+class ImageHandler (py.ImageEventHandler):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.time_old = time.time()
+
+        self.converter = py.ImageFormatConverter()
+        self.converter.OutputPixelFormat = py.PixelType_RGB8packed
+        self.converter.OutputBitAlignment = "MsbAligned"
+    
+    def OnImageGrabbed(self, camera, grabResult):
+        try:
+            if grabResult.GrabSucceeded():
+                
+                if (~self.converter.ImageHasDestinationFormat(grabResult)):
+                    grabResult = self.converter.Convert(grabResult)
+                    
+                img = grabResult.Array
+                time_new = time.time()
+                rate = 1/(time_new-self.time_old)
+                print(rate)
+                self.time_old = time_new
+                if (should_feed_video):
+                    image_feed(img)
+                if (should_visualize):
+                    visualize(img)
+            else:
+                raise RuntimeError("Grab failed")
+        except Exception as e:
+            traceback.print_exc()
+
+def BackgroundLoop(cam):
+    handler = ImageHandler()
+
+    cam.RegisterImageEventHandler(handler, py.RegistrationMode_ReplaceAll, py.Cleanup_None)
+
+    global feeder
+    with FFMPEG_Image_Feeder((cam.Height.Value, cam.Width.Value), fps=fps, pixfmt="yuv420p", codec="h264_qsv", quality= str(quality_factor), preset= 'fast') as feeder:
+        # cam.StartGrabbingMax(100, py.GrabStrategy_LatestImages, py.GrabLoop_ProvidedByInstantCamera)
+        cam.StartGrabbing(py.GrabStrategy_LatestImageOnly, py.GrabLoop_ProvidedByInstantCamera)
+        # cam.StartGrabbing(py.GrabStrategy_LatestImages, py.GrabLoop_ProvidedByInstantCamera)
+
+        try:
+            while cam.IsGrabbing():
+                pass
+        except KeyboardInterrupt:
+            pass
+
+        cam.StopGrabbing()
+        cam.DeregisterImageEventHandler(handler)
+        cam.Close()
+        cv2.destroyAllWindows()
+
+    # return handler.img_sum
+
+
+tlf = py.TlFactory.GetInstance()
+cam = py.InstantCamera(tlf.CreateFirstDevice())
+cam.Open()
+
+camera_name = cam.DeviceInfo.GetUserDefinedName()
+print(f"connected to {camera_name} camera")
+
+initialize_cam(cam, camera_name)
+
+BackgroundLoop(cam)
--- camera_rviz_connect/scripts/test_camera/test_ffmpeg.py
+++ camera_rviz_connect/scripts/test_camera/test_ffmpeg.py
@@ -1,42 +1,29 @@
 # import subprocess , os
 import subprocess as sp
+from sys import stderr, stdout
+import numpy as np
+import cv2
+
+# p1 = sp.Popen('ffmpeg -i input.jpg -f image2pipe -pix_fmt bgr24 -vcodec rawvideo -',shell=True , stdout = sp.PIPE, stderr =  sp.PIPE, text=True)
+
+# p1.wait()
+
+# a =p1.stdout.readlines(1)
+# print(a)
 
 
-# inVid = "input.mp4"
-# outVid = "output.mp4"
+command = ['ffmpeg',
+           '-i', 'test.jpg',
+           '-f', 'image2pipe',
+           '-pix_fmt', 'bgr24',
+           '-vcodec', 'rawvideo', '-an', '-']
 
-# cmd = [
-#     "ffmpeg",
-#     '-y',
-#     # '-loglevel', 'error' if logfile == sp.PIPE else 'info',
-#     '-f', 'rawvideo',
-#     '-vcodec', 'rawvideo',
-#     '-s', '%dx%d' % (1920, 1080),
-#     '-pix_fmt', 'rgba',
-#     '-r', '%.02f' % 30,
-#     '-i', '-', '-an',
-# ]
-# cmd.extend([
-#     '-vcodec', "h264_qsv",
-#     '-q:v', "30",
-#     # '-crf', crf,
-#     '-preset', "medium",
-# ])
+p1 = sp.Popen(command, stdout=sp.PIPE, stderr = sp.DEVNULL)
 
-# cmd.extend([
-#     "output.mp4"
-# ])
+raw_frame = p1.stdout.read(1920*1080*3)
+frame = np.frombuffer(raw_frame, np.uint8)
+frame = frame.reshape((1080, 1920, 3))
 
-# popen_params = {"stdout": sp.DEVNULL,
-#                     "stderr": None,
-#                     "stdin": sp.PIPE,
-#                     "shell":True}
-# proc = sp.Popen(cmd, **popen_params)
-# proc = subprocess.Popen('ffmpeg -i input.mp4 ' + '''-vf "[in]drawtext=fontsize=20:fontcolor=White:fontfile='/Windows/Fonts/arial.ttf':text='onLine1':x=(w)/2:y=(h)/2, drawtext=fontsize=20:fontcolor=White:fontfile='/Windows/Fonts/arial.ttf':text='onLine2':x=(w)/2:y=((h)/2)+25, drawtext=fontsize=20:fontcolor=White:fontfile='/Windows/Fonts/arial.ttf':text='onLine3':x=(w)/2:y=((h)/2)+50[out]" -y test_out.avi''' + outVid , shell=True, stderr=subprocess.PIPE)
-# proc.wait()
-# print( proc.stderr.read())
-# os.startfile( outVid )
-# proc = subprocess.Popen("ffmpeg" + " -i " + inVid + " -vf drawtext=fontfile='C\:\\Windows\\Fonts\\arial.ttf'|text='test' -y " + outVid , shell=True, stderr=subprocess.PIPE)
-proc = sp.Popen("ffmpeg -i input.mp4 -vf \"drawtext=text='Test Text':fontcolor=red:fontsize=75:x=1002:y=100:\" output.mp4" , shell=True, stderr=sp.PIPE)
-proc.wait()
-print( proc.stderr.read())
+
+cv2.imshow('image', frame)
+cv2.waitKey(0)
