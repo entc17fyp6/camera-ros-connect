059017d0da96fe8fc1904f0b860220c51baa9ea3
tharinduSamare <tharindu.samare123@gmail.com>
2022-02-17 13:46:25 +0530

    added feature: redesigned multi-camera image feeder with c++

--- camera_rviz_connect/src/multi_camera_image_feeder.cpp
+++ camera_rviz_connect/src/multi_camera_image_feeder.cpp
@@ -20,13 +20,15 @@ using namespace std;
 uint32_t width = 1920;
 uint32_t height = 1080;
 uint8_t fps = 30;
-uint16_t narrow_AutoExposureTimeUpperLimit = 1000;
+uint16_t narrow_AutoExposureTimeUpperLimit = 10000;
 uint16_t wide_AutoExposureTimeUpperLimit = 10000;
 String_t PixelFormat = "YCbCr422_8" ;
 
+intptr_t narrow_cam_id = 0;
+intptr_t wide_cam_id = 1;
 
 void Initialize_cam(CInstantCamera& camera);
-void background_loop(CInstantCamera& camera);
+void background_loop(CInstantCameraArray& cameras);
 void visualize(cv::Mat& frame);
 
 int main( int /*argc*/, char* /*argv*/[] )
@@ -40,22 +42,35 @@ int main( int /*argc*/, char* /*argv*/[] )
 
     try
     {
+        // Get the transport layer factory.
+        CTlFactory& tlFactory = CTlFactory::GetInstance();
 
-        // Create an instant camera object with the camera found first.
-        CInstantCamera camera( CTlFactory::GetInstance().CreateFirstDevice() );
+        // Get all attached devices and exit application if no device is found.
+        DeviceInfoList_t devices;
+        if (tlFactory.EnumerateDevices( devices ) == 0)
+        {
+            throw RUNTIME_EXCEPTION( "No camera present." );
+        }
 
-        camera.Open();
-        Initialize_cam(camera);
-        INodeMap& nodemap = camera.GetNodeMap();
-        cout << "chunk enable" << camera.ChunkNodeMapsEnable.GetValue() << endl;
-        camera.ChunkNodeMapsEnable.SetValue(false);
-        cout << "chunk enable_2" << camera.ChunkNodeMapsEnable.GetValue() << endl;
+        // Create an array of instant cameras for the found devices and avoid exceeding a maximum number of devices.
+        CInstantCameraArray cameras(devices.size());
+
+        // Create and attach all Pylon Devices.
+        for (size_t i = 0; i < cameras.GetSize(); ++i)
+        {
+            cameras[i].Attach( tlFactory.CreateDevice( devices[i] ) );
+            Initialize_cam(cameras[i]);
+
+            // Print the model name of the camera.
+            cout << "Using device " << cameras[i].GetDeviceInfo().GetUserDefinedName() << endl;
+        }
 
 
 
-        background_loop(camera);
 
-        camera.Close();
+        background_loop(cameras);
+
+        // cameras.Close();
         PylonTerminate();
     }
     catch (const GenericException& e)
@@ -78,6 +93,9 @@ int main( int /*argc*/, char* /*argv*/[] )
 
 void Initialize_cam(CInstantCamera& camera){
     String_t cam_name = camera.GetDeviceInfo().GetUserDefinedName();
+    if (camera.IsOpen() == false){
+        camera.Open();
+    }
     camera.ChunkNodeMapsEnable.SetValue(false);
 
     INodeMap& nodemap = camera.GetNodeMap();
@@ -94,19 +112,20 @@ void Initialize_cam(CInstantCamera& camera){
 
     if (cam_name == "Wide"){
         CFloatParameter(nodemap,"AutoExposureTimeUpperLimit").SetValue(wide_AutoExposureTimeUpperLimit);
+        camera.SetCameraContext(wide_cam_id);
     }
     else if (cam_name == "Narrow"){
         CFloatParameter(nodemap,"AutoExposureTimeUpperLimit").SetValue(narrow_AutoExposureTimeUpperLimit);
         CBooleanParameter(nodemap, "ReverseX").SetValue(true);
         CBooleanParameter(nodemap, "ReverseY").SetValue(true);
-
+        camera.SetCameraContext(narrow_cam_id);
     }
 
 }
 
-void visualize(cv::Mat& frame){ 
+void visualize(cv::Mat& frame, intptr_t cam_id){ 
     cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
-    string windowName = "frame"; //Name of the window
+    string windowName = "frame_"+to_string(cam_id); //Name of the window
     cv::namedWindow(windowName); // Create a window
     cv::imshow(windowName, frame);
     cv::waitKey(1);
@@ -129,17 +148,23 @@ public:
     }
     void OnImageGrabbed( CInstantCamera& camera, const CGrabResultPtr& ptrGrabResult )
     {
-        cout << "OnImageGrabbed event for device " << camera.GetDeviceInfo().GetUserDefinedName()<< endl;
+        intptr_t cam_id = camera.GetCameraContext();
         cv::Mat image;
         if (ptrGrabResult->GrabSucceeded()){
 
             format_converter.Convert(image_converted, ptrGrabResult);
             image = cv::Mat(height, width, CV_8UC3, (uint8_t *) image_converted.GetBuffer());
-            visualize(image);
+            visualize(image,cam_id);
             time_new = chrono::steady_clock::now();
             duration = chrono::duration_cast<std::chrono::microseconds>(time_new - time_old).count();  //micro seconds
             time_old = time_new;
-            cout << "rate = " << 1000000/(duration) << endl;  //Hz
+            if (cam_id == narrow_cam_id){
+                cout << "\t\t\t\t\t\tnarrow rate = " << 1000000/(duration) << endl;  //Hz
+
+            }
+            else{
+                cout << "wide rate = " << 1000000/(duration) << endl;  //Hz
+            }
 
         }
         else{
@@ -148,24 +173,35 @@ public:
     }
 };
 
-void background_loop(CInstantCamera& camera){
+void background_loop(CInstantCameraArray& cameras){
 
-    CImageEventHandler* image_handler = new ImageHandler;
-    camera.RegisterImageEventHandler( image_handler, RegistrationMode_Append, Cleanup_Delete );
-    camera.StartGrabbing( GrabStrategy_LatestImageOnly, GrabLoop_ProvidedByInstantCamera );
+    uint8_t cam_count = cameras.GetSize();
+    CImageEventHandler* image_handlers [cam_count];
+
+    for (uint8_t i=0;i<cam_count;i++){
+        image_handlers[i] = new ImageHandler;
+        cameras[i].RegisterImageEventHandler(image_handlers[i], RegistrationMode_Append, Cleanup_Delete);
+    }
+    cameras.StartGrabbing(GrabStrategy_LatestImageOnly, GrabLoop_ProvidedByInstantCamera);
+    // CImageEventHandler* image_handler = new ImageHandler;
+    // camera.RegisterImageEventHandler( image_handler, RegistrationMode_Append, Cleanup_Delete );
+    // camera.StartGrabbing( GrabStrategy_LatestImageOnly, GrabLoop_ProvidedByInstantCamera );
 
     try{
-
-        while (camera.IsGrabbing()){
+        while (cameras.IsGrabbing()){
             continue;
         }
     }
     catch (const GenericException& e) {
         // Error handling.
         cerr << "An exception occurred." << endl << e.GetDescription() << endl;
-        camera.StopGrabbing();
-        camera.DeregisterImageEventHandler(image_handler);
-        camera.Close();
+        cameras.StopGrabbing();
+        for(uint8_t i=0;i<cam_count;i++){
+            cameras[i].DeregisterImageEventHandler(image_handlers[i]);
+            cameras[i].Close();
+        }
+        // camera.DeregisterImageEventHandler(image_handler);
+        // camera.Close();
         cv::destroyAllWindows();
     }
     
